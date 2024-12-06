51d3a508af4941660d9881b57bace4dcceec8957
cyunlee <siyunkr@gmail.com>
2024-12-06 13:03:18 +0900

    fix: MenuGenerator 수정

--- src/main/java/menu/domain/MenuGenerator.java
+++ src/main/java/menu/domain/MenuGenerator.java
@@ -3,11 +3,11 @@ package menu.domain;
 import java.util.List;
 
 public class MenuGenerator {
-    public Menus generatorMenu() {
+    public Menus generateMenu(String category, String name) {
         Menus menus = new Menus();
 
-        for(int i=0; i<menus.size(); i++) {
-            Menu menu = new Menu();
+        for(int i=0; i<menus.getSize(); i++) {
+            Menu menu = new Menu(category, name);
             menus.addMenu(menu);
         }
         return menus;
