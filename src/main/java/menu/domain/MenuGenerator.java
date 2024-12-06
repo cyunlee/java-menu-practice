0335dd61c3c3ee1a878007f536137fbc0f14c14f
cyunlee <siyunkr@gmail.com>
2024-12-06 13:18:30 +0900

    feat: md 파일 src 값 담는 상수 객체 구현

--- src/main/java/menu/domain/MenuGenerator.java
+++ src/main/java/menu/domain/MenuGenerator.java
@@ -11,12 +11,6 @@ import menu.util.FileParser;
 
 public class MenuGenerator {
     private final FileParser fileParser = new FileParser();
-    private final static String ASIAN_PATH = "src/main/java/menu/AsianFood.md";
-    private final static String CHINESE_PATH = "src/main/java/menu/ChineseFood.md";
-    private final static String JAPANESE_PATH = "src/main/java/menu/JapaneseFood.md";
-    private final static String KOREAN_PATH = "src/main/java/menu/KoreanFood.md";
-    private final static String WESTERN_PATH = "src/main/java/menu/WesternFood.md";
-
     public Menus generateMenu(String filePath) {
         Map<String, List<String>> parsedMenu = parseFromFile(filePath);
         String parsedCategory = "";
