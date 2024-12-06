7cbd07318ba192cb967b1c2fd913e641bd373464
cyunlee <siyunkr@gmail.com>
2024-12-06 13:20:33 +0900

    feat: Constants 상수 값 public으로 변경

--- src/main/java/menu/common/Constants.java
+++ src/main/java/menu/common/Constants.java
@@ -1,10 +1,10 @@
 package menu.common;
 
 public class Constants {
-    private final static String ASIAN_PATH = "src/main/java/menu/AsianFood.md";
-    private final static String CHINESE_PATH = "src/main/java/menu/ChineseFood.md";
-    private final static String JAPANESE_PATH = "src/main/java/menu/JapaneseFood.md";
-    private final static String KOREAN_PATH = "src/main/java/menu/KoreanFood.md";
-    private final static String WESTERN_PATH = "src/main/java/menu/WesternFood.md";
+    public final static String ASIAN_PATH = "src/main/java/menu/AsianFood.md";
+    public final static String CHINESE_PATH = "src/main/java/menu/ChineseFood.md";
+    public final static String JAPANESE_PATH = "src/main/java/menu/JapaneseFood.md";
+    public final static String KOREAN_PATH = "src/main/java/menu/KoreanFood.md";
+    public final static String WESTERN_PATH = "src/main/java/menu/WesternFood.md";
 
 }
--- src/main/java/menu/controller/MenuPickerController.java
+++ src/main/java/menu/controller/MenuPickerController.java
@@ -1,14 +1,23 @@
 package menu.controller;
 
+import menu.common.Constants;
+import menu.domain.MenuGenerator;
+import menu.domain.Menus;
 import menu.view.InputView;
 import menu.view.OutputView;
 
 public class MenuPickerController {
+    private final MenuGenerator menuGenerator = new MenuGenerator();
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void run() {
         String input = requireInput();
+        Menus asianMenus = generateMenus(Constants.)
+    }
+
+    public Menus generateMenus(String filePath) {
+        return menuGenerator.generateMenu(filePath);
     }
 
     private String requireInput() {
